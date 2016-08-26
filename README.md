20e81098987e2e0bbd5edd512b08aeb89dde0b8a
IgorYarutkin <igor.yarutkin@gmail.com>
2016-08-26 17:11:06 +0300

    Редизайн

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# IgorYarutkin.github.io
-Igor Yarutkin's GitHub site 
+# IgorYarutkin.github.io [![Build status][travis-image]][travis-url] [![Dependency status][dependency-image]][dependency-url]
+Igor Yarutkin's GitHub site
 
+[travis-image]: https://travis-ci.org/IgorYarutkin/IgorYarutkin.github.io.svg?branch=master
+[travis-url]: https://travis-ci.org/IgorYarutkin/IgorYarutkin.github.io
+[dependency-image]: https://david-dm.org/IgorYarutkin/IgorYarutkin.github.io.svg?style=flat-square
+[dependency-url]: https://IgorYarutkin/IgorYarutkin.github.io
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var server = require('browser-sync');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var atImport = require('postcss-import');
+var del = require('del');
+var runSequence = require('run-sequence');
+var normalize = require('postcss-normalize');
+
+gulp.task('html', function buildHTML() {
+  return gulp.src('src/*.pug')
+    .pipe(pug({}))
+    .pipe(gulp.dest('build'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('style', function() {
+  return gulp.src('src/css/style.css')
+    .pipe(postcss([
+      atImport(),
+      normalize(),
+      cssnext()
+    ]))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*')
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('clean', function() {
+  return del('build/**/*');
+});
+
+gulp.task('serve', function() {
+  runSequence('clean', ['html', 'style', 'images']);
+  server.init({
+    server: 'build',
+    notify: false,
+    open: false,
+    ui: false
+  });
+  gulp.watch('src/*.pug', ['html']);
+  gulp.watch('src/**/*.css', ['style']);
+  gulp.watch('src/img/*', ['images']);
+});
